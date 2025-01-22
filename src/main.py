c9cc0b3d442d18f656cc0efc5a2d04d25760bffd
nordluma <maxnordlund.95@hotmail.com>
2025-01-22 21:27:38 +0200

    feat(main): draw a line

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
-from graphics import Window
+from graphics import Line, Point, Window
 
 
 def main():
     win = Window(800, 600)
+
+    line1 = Line(Point(100, 100), Point(200, 300))
+    win.draw_line(line1, "black")
+
     win.wait_for_close()
 
 
