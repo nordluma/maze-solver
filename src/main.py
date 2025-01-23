e45bd7228b1f85b6a40f1f0dd8b5ca3d2e806322
nordluma <maxnordlund.95@hotmail.com>
2025-01-23 21:34:37 +0200

    feat(cell): create class to draw cells

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
-from graphics import Line, Point, Window
+from cell import Cell
+from graphics import Window
 
 
 def main():
     win = Window(800, 600)
 
-    line1 = Line(Point(100, 100), Point(200, 300))
-    win.draw_line(line1, "black")
+    cell = Cell(win)
+    cell.has_left_wall = False
+    cell.draw(50, 50, 100, 100)
+
+    cell = Cell(win)
+    cell.has_right_wall = False
+    cell.draw(125, 125, 200, 200)
+
+    cell = Cell(win)
+    cell.has_bottom_wall = False
+    cell.draw(225, 225, 250, 250)
+
+    cell = Cell(win)
+    cell.has_top_wall = False
+    cell.draw(300, 300, 500, 500)
 
     win.wait_for_close()
 
