44e3ff4206a73a606026711998d54b3310adf6bf
nordluma <maxnordlund.95@hotmail.com>
2025-01-23 22:04:22 +0200

    feat(cell): draw a line between cells

--- src/cell.py
+++ src/cell.py
@@ -27,3 +27,25 @@ class Cell:
             self._win.draw_line(Line(Point(x2, y1), Point(x2, y2)))
         if self.has_bottom_wall:
             self._win.draw_line(Line(Point(x1, y2), Point(x2, y2)))
+
+    def draw_move(self, to_cell, undo: bool = False):
+        x_center, y_center = self._get_center()
+        x_center_2, y_center_2 = to_cell._get_center()
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        self._win.draw_line(
+            Line(Point(x_center, y_center), Point(x_center_2, y_center_2)),
+            fill_color,
+        )
+
+    def _get_center(self) -> tuple[int, int]:
+        if not self._x1 or not self._x2 or not self._y1 or not self._y2:
+            raise Exception("Cell has to be drawn before drawing a move")
+        half_len = abs(self._x2 - self._x1) // 2
+        x_center = half_len + self._x1
+        y_center = half_len + self._y1
+
+        return x_center, y_center
