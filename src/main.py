829247ababa6f7e9a7b3783f0e885082a05484c5
nordluma <maxnordlund.95@hotmail.com>
2025-01-23 22:04:45 +0200

    feat(main): test drawing lines between cells

--- src/main.py
+++ src/main.py
@@ -5,21 +5,29 @@ from graphics import Window
 def main():
     win = Window(800, 600)
 
-    cell = Cell(win)
-    cell.has_left_wall = False
-    cell.draw(50, 50, 100, 100)
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
 
-    cell = Cell(win)
-    cell.has_right_wall = False
-    cell.draw(125, 125, 200, 200)
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
 
-    cell = Cell(win)
-    cell.has_bottom_wall = False
-    cell.draw(225, 225, 250, 250)
+    c1.draw_move(c2)
 
-    cell = Cell(win)
-    cell.has_top_wall = False
-    cell.draw(300, 300, 500, 500)
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4)
 
     win.wait_for_close()
 
