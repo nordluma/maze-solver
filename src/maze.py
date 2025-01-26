fb9e98a1f9fdca99fe6aac794234a5f788c542be
nordluma <maxnordlund.95@hotmail.com>
2025-01-26 21:19:01 +0200

    feat(maze): add logic for generating a random maze

--- src/maze.py
+++ src/maze.py
@@ -1,4 +1,5 @@
 import time
+import random
 from typing import Union
 
 from cell import Cell
@@ -12,9 +13,10 @@ class Maze:
         y1: int,
         num_rows: int,
         num_cols: int,
-        cell_size_x: int,
-        cell_size_y: int,
+        cell_size_x: float,
+        cell_size_y: float,
         win: Union[Window, None] = None,
+        seed: Union[int, None] = None,
     ) -> None:
         self._x1 = x1
         self._y1 = y1
@@ -25,8 +27,12 @@ class Maze:
         self._cells = []
         self._win = win
 
+        if seed:
+            random.seed(seed)
+
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
 
     def _create_cells(self):
         for i in range(self.num_cols):
@@ -68,3 +74,41 @@ class Maze:
         max_row = self.num_rows - 1
         self._cells[max_col][max_row].has_bottom_wall = False
         self._draw_cell(max_col, max_row)
+
+    def _break_walls_r(self, i, j):
+        current_cell = self._cells[i][j]
+        current_cell.visited = True
+
+        while True:
+            next_idx_list = []
+            if i > 0 and not self._cells[i - 1][j].visited:  # North
+                next_idx_list.append((i - 1, j))
+            if i < self.num_cols - 1 and self._cells[i + 1][j].visited:  # South
+                next_idx_list.append((i + 1, j))
+            if j > 0 and not self._cells[i][j - 1].visited:  # West
+                next_idx_list.append((i, j - 1))
+            if j < self.num_rows - 1 and not self._cells[i][j + 1].visited:  # East
+                next_idx_list.append((i, j + 1))
+
+            if len(next_idx_list) == 0:  # no directions to go to
+                self._draw_cell(i, j)
+                return
+
+            direction_idx = random.randrange(len(next_idx_list))
+            ni, nj = next_idx_list[direction_idx]
+            next_cell = self._cells[ni][nj]
+
+            if ni == i + 1:
+                current_cell.has_right_wall = False
+                next_cell.has_left_wall = False
+            if ni == i - 1:
+                current_cell.has_left_wall = False
+                next_cell.has_right_wall = False
+            if nj == j + 1:
+                current_cell.has_bottom_wall = False
+                next_cell.has_top_wall = False
+            if nj == j - 1:
+                current_cell.has_top_wall = False
+                next_cell.has_bottom_wall = False
+
+            self._break_walls_r(ni, nj)
