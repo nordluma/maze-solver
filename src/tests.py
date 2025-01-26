c40d96fa964213d3f5c00321822e732ce77a7dca
nordluma <maxnordlund.95@hotmail.com>
2025-01-26 12:12:42 +0200

    test(maze): test cell creation with larger amount of cells

--- src/tests.py
+++ src/tests.py
@@ -11,6 +11,13 @@ class Tests(unittest.TestCase):
         self.assertEqual(len(m1._cells), num_cols)
         self.assertEqual(len(m1._cells[0]), num_rows)
 
+    def test_maze_create_cells_many(self):
+        num_cols = 20
+        num_rows = 16
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(len(m1._cells), num_cols)
+        self.assertEqual(len(m1._cells[0]), num_rows)
+
 
 if __name__ == "__main__":
     unittest.main()
