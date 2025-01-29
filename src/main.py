f4d1ceb5d77a49688804312a40d312ba0404d1a7
nordluma <maxnordlund.95@hotmail.com>
2025-01-29 23:08:50 +0200

    feat(main): run the maze solver

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import sys
+
 from graphics import Window
 from maze import Maze
 
@@ -10,8 +12,25 @@ def main():
     screen_y = 600
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
+
+    sys.setrecursionlimit(10_000)
     win = Window(screen_x, screen_y)
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+
+    maze = Maze(
+        margin,
+        margin,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    )
+    print("maze created")
+    solved = maze.solve()
+    if solved:
+        print("maze solved!")
+    else:
+        print("maze cannot be solved!")
 
     win.wait_for_close()
 
